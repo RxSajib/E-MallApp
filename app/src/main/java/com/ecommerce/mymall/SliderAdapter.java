6cf2ec689bceb7f8f33fae8e62c494260ce70328
sajib_roy <sajibroy206@gmail.com>
2019-12-26 21:00:34 +0600

    sajibkjb

---
+++ app/src/main/java/com/ecommerce/mymall/SliderAdapter.java
@@ -0,0 +1,40 @@
+package com.ecommerce.mymall;
+
+import android.print.PageRange;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class SliderAdapter  extends PagerAdapter {
+
+    private List<slider_model> slider_models;
+
+    public SliderAdapter(List<slider_model> slider_models) {
+        this.slider_models = slider_models;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.destroyItem(container, position, object);
+    }
+}
---
+++ app/src/main/java/com/ecommerce/mymall/slider_model.java
@@ -0,0 +1,18 @@
+package com.ecommerce.mymall;
+
+public class slider_model {
+
+    private int banner;
+
+    public slider_model(int banner) {
+        this.banner = banner;
+    }
+
+    public int getBanner() {
+        return banner;
+    }
+
+    public void setBanner(int banner) {
+        this.banner = banner;
+    }
+}
